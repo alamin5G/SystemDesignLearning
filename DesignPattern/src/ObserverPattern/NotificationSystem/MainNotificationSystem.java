a22608da6318414a6e016b2bc7d9c6c0fc7dcb41
Md Al Amin <alaminrakib4864@gmail.com>
2026-02-05 04:49:11 +0600

    Implement main method to demonstrate student registration and notification

--- DesignPattern/src/ObserverPattern/NotificationSystem/MainNotificationSystem.java
+++ DesignPattern/src/ObserverPattern/NotificationSystem/MainNotificationSystem.java
@@ -1,4 +1,20 @@
 package ObserverPattern.NotificationSystem;
 
 public class MainNotificationSystem {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Alamin", "malaminswe@gmail.com", "01712345678");
+        Student student2 = new Student("Jahid", "jahid@gmail.com", "01812345678");
+
+        StudentService studentService = new StudentService();
+        studentService.registerObserver(new EmailNotificationObserverImpl());
+        studentService.registerObserver(new SmsNotificationObserverImpl());
+        studentService.registerStudent(student1);
+        studentService.registerStudent(student2);
+
+
+
+    }
+
+
 }
